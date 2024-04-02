ccd07f8098fce9ebf7b5ffaca23f2cae80cb554d
Justice Dixon <justicedxn1@gmail.com>
2024-04-02 00:31:37 -0700

    fixed text overlay on model page. Hid model page from nav bar for now

--- src/pages/ModellingPage.js
+++ src/pages/ModellingPage.js
@@ -4,32 +4,42 @@ import React, { useState } from 'react';
 
 function ModellingPage() {
     const [hoveredIndex, setHoveredIndex] = useState(null); // State to track hovered image index
+    const [isTextVisible, setIsTextVisible] = useState(false); // State to track text visibility
 
     const images = Object.keys(modelData);
 
-    return(
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+        setIsTextVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+        setIsTextVisible(false);
+    };
+
+    return (
         <div className='model-format'>
             <Row className='model-row mx-auto'>
                 {images.map((index) => (
-                        <div
-                            key={index}
-                            className="image-container"
-                        >
-                            <img 
-                            onMouseEnter={() => setHoveredIndex(index)} // Set hovered index on mouse enter
-                            onMouseLeave={() => setHoveredIndex(null)} // Reset hovered index on mouse leave
-                            className="model-image mx-auto" src={modelData[index].picture} alt={modelData[index].location} />
-                            {hoveredIndex === index && ( // Show text only if hoveredIndex matches the current index
-                                <div className="image-text">
-                                    {modelData[index].location}<br></br>
-                                    {modelData[index].date}
-                                </div>
-                            )}
-                        </div>
+                    <div
+                        key={index}
+                        className="model-image-container"
+                        onMouseEnter={() => handleMouseEnter(index)} // Set hovered index on mouse enter
+                        onMouseLeave={handleMouseLeave} // Reset hovered index on mouse leave
+                    >
+                        <img className="model-image mx-auto" src={modelData[index].picture} alt={modelData[index].location} />
+                        {hoveredIndex === index && isTextVisible && ( // Show text only if hoveredIndex matches the current index and isTextVisible is true
+                            <div className="model-image-text">
+                                {modelData[index].location}<br></br>
+                                {modelData[index].date}
+                            </div>
+                        )}
+                    </div>
                 ))}
             </Row>
             <div className='model-measurements'>
-                <hr className='model-line'/>
+                <hr className='model-line' />
                 <b>Height: </b> 6' 1.5"&nbsp;&nbsp;&nbsp;
                 <b>Hair: </b> Dark Brown&nbsp;&nbsp;&nbsp;
                 <b>Eyes: </b> Brown&nbsp;&nbsp;&nbsp;
@@ -38,7 +48,6 @@ function ModellingPage() {
                 <b>Inseam: </b> 32"&nbsp;&nbsp;&nbsp;
                 <b>Shoe: </b> 13
             </div>
-
         </div>
     );
 }
