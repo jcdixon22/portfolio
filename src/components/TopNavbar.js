74248dbbfd98e65399713440cc80fdcce9a04671
justice.dixon <Justice.Dixon@PACONSULTING.COM>
2024-04-02 14:32:52 -0700

    Testing another headshot pic

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,18 +1,33 @@
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import React, { useEffect, useState} from 'react';
 
 
 function TopNavbar() {
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setWindowWidth(window.innerWidth);
+  //   };
+
+  //   window.addEventListener('resize', handleResize);
+
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
+
     return (
       <Navbar collapseOnSelect expand="sm" data-bs-theme="dark">
-          <Nav>
-            <Nav.Link href="/">
-              <div className='navName'>
-                <b>Justice Dixon</b>
-              </div>
-            </Nav.Link>
-          </Nav>
+        <Nav className='navlogo'>
+          <Nav.Link href="/">
+            <div className='navName'>
+              <b>Justice Dixon</b>
+            </div>
+          </Nav.Link>
+        </Nav>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className='nav-hamburger'/>
         <Navbar.Collapse id="responsive-navbar-nav" className='navElements navbar-light'>
           <Nav className="navVertAlign">
