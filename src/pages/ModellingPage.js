5c84112325a391d750dd0af7254c566745bc72c2
Justice Dixon <justicedxn1@gmail.com>
2024-08-05 20:05:51 -0700

    temporary model page changes

--- src/pages/ModellingPage.js
+++ src/pages/ModellingPage.js
@@ -2,6 +2,20 @@ import modelData from '../model_data.json'
 import { Row } from 'react-bootstrap';
 import React, { useState } from 'react';
 
+import newport from '../model_images/newport.jpg';
+import nyc from '../model_images/nyc.jpg';
+import palo_alto_1 from '../model_images/palo_alto_1.jpg';
+import palo_alto_2 from '../model_images/palo_alto_2.jpg';
+import palo_alto_3 from '../model_images/palo_alto_3.jpg';
+import palo_alto_4 from '../model_images/palo_alto_4.jpg';
+import palo_alto_5 from '../model_images/palo_alto_5.jpg';
+import palo_alto_6 from '../model_images/palo_alto_6.jpg';
+import palo_alto_7 from '../model_images/palo_alto_7.jpg';
+import sutro_1 from '../model_images/sutro_1.jpg';
+import sutro_2 from '../model_images/sutro_2.jpg';
+import sutro_3 from '../model_images/sutro_3.jpg';
+import tel_aviv from '../model_images/tel_aviv.jpg';
+
 function ModellingPage() {
     const [hoveredIndex, setHoveredIndex] = useState(null); // State to track hovered image index
     const [isTextVisible, setIsTextVisible] = useState(false); // State to track text visibility
@@ -21,7 +35,7 @@ function ModellingPage() {
     return (
         <div className='model-format'>
             <Row className='model-row mx-auto'>
-                {images.map((index) => (
+                {/* {images.map((index) => (
                     <div
                         key={index}
                         className="model-image-container"
@@ -36,7 +50,48 @@ function ModellingPage() {
                             </div>
                         )}
                     </div>
-                ))}
+                ))} */}
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={newport}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={nyc}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_1}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_2}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_3}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_4}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_5}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_6}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={palo_alto_7}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={sutro_1}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={sutro_2}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={sutro_3}/>
+                </div>
+                <div className="model-image-container">
+                    <img className="model-image mx-auto" src={tel_aviv}/>
+                </div>
+
+
             </Row>
             <div className='model-measurements'>
                 <hr className='model-line' />
